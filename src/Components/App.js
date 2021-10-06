098118705516bf0ffd7b0489e54b7404a1ced1cd
Sahil Khan <thesahindia@gmail.com>
2021-10-06 23:16:10 +0530

    routes setup

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
-import Homepage from './HomePage';
-
+import React, { useState } from "react";
+import Homepage from "./HomePage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SignIn from "./sign-up-in/signIn";
+import SignUp from "./sign-up-in/SignUp";
 const App = () => {
-
-	return (
-		<div>
-			<Homepage />
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Homepage}></Route>
+          <Route path="/signin" exact component={SignIn}></Route>
+          <Route path="/signup" exact component={SignUp}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
