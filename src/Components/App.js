453b4fcc147d467eb9ce4a510c50901829265f03
Avinesh Tripathi <avineshtripathi1@gmail.com>
2021-10-07 14:37:43 +0530

    added Profile

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import Homepage from './HomePage';
+import Profile from './Profile';
+import SignIn from './sign-up-in/signIn';
+import SignUp from './sign-up-in/SignUp';
 
 const App = () => {
 
